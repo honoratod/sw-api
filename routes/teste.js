c45587c8ca2607b2b882361be4cceafa81a5d115
honoratod <honorato.uva@gmail.com>
2019-06-14 14:37:08 -0300

    Teste de loop async
    Teste de loop de chamada assincrona para adicionar dados ao objeto.

--- routes/teste.js
+++ routes/teste.js
@@ -27,4 +27,72 @@ router.get("/testeSw", function (req, res) {
     })
 });
 
+router.get("/testefor", function (req, res) {
+
+    let planetasLista = [
+        {
+            nome: 'teste1',
+            clima: 'clima1',
+            id: 4
+        }, {
+            nome: 'teste2',
+            clima: 'clima1',
+            id: 6
+        }, {
+            nome: 'teste3',
+            clima: 'clima1',
+            id: 7
+        }
+    ];
+
+    //Array de promessas
+    var promiseArray = [];
+    //Executa o loop para obter os valores de quantidade de aparições para cada planeta
+    for (let i = 0; i < planetasLista.length; i++) {
+        promiseArray.push(retornaQtdFilmes(planetasLista[i].id)
+            .then((title) => {
+                //Adiciona a informação obtida
+                console.log("resultado1");
+                planetasLista[i]["title"] = title;
+            })
+            .catch(err => {
+                console.log("Err:" + err)
+                planetasLista[i]["title"] = '';
+            })
+        )
+    }
+
+    //Aguarda a listagem de todas as quantidades de aparições (retorno de todas as promises)
+    Promise.all(promiseArray).then((title) => {
+        console.log("await");
+        res.status(200).json(planetasLista);
+    }).catch(err => {
+        res.status(200).json(planetasLista);
+    })
+});
+
+function retornaQtdFilmes(nome) {
+    options = {
+        url: 'http://jsonplaceholder.typicode.com/todos/' + nome,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'content-type': 'application/json',
+        }
+    };
+
+    return new Promise(function (resolve, reject) {
+        request(options, function (error, response, body) {
+            if (error || response.statusCode != 200) {
+                reject('Desconhecido');
+            } else {
+                let infoPlaneta = JSON.parse(body);
+                resolve(infoPlaneta.title);
+            }
+        })
+    })
+
+}
+
+
 module.exports = router;
